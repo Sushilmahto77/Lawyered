5eac326635bd48c5caf698d9ae6c37cb8d1516de
Sushil Kumar Mahto <74419540+Sushil-mahto@users.noreply.github.com>
2023-08-22 18:09:16 +0530

    Initial Commit

---
+++ Lawyered/LawyeredApp.swift
@@ -0,0 +1,17 @@
+//
+//  LawyeredApp.swift
+//  Lawyered
+//
+//  Created by Sushil Mahto on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LawyeredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
